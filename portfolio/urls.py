bb151ac6ca2443e7df03ca6b945f4411cb69a97a
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-05-25 15:10:58 +0300

    ts:template rendering - tests if the application view gets rendered

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import re_path as url, include
+
+import portapp
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
+    url('', include('portapp.urls'))
 ]
