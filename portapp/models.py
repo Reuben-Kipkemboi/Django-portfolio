298dc6d4929742063fd32a00d5b068b82fad5aeb
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-05-26 17:40:42 +0300

    ft:simple-models - creation of simple models

--- portapp/models.py
+++ portapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    
+    def __str__(self):
+        return self.first_name
+    #the str is just a basic way that helps if we want to have a view of anything that we would like to query
+    
+    
+    
+    
+    
+class Project(models.Model):
+    name = models.CharField(max_length =60)
+    description = models.TextField()
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+
